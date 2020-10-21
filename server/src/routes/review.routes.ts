7629e2afa9df2b5ce2dbd99e602d14f5465dce58
destroT <destro.tommaso@gmail.com>
2020-10-21 17:13:03 +0200

    Changed route for Review from api/review/getAll to api/review/

--- server/src/routes/review.routes.ts
+++ server/src/routes/review.routes.ts
@@ -5,7 +5,7 @@ import validateId from '../middlewares/validateId';
 const router = Router();
 
 router.post('/add', isAuth, add);
-router.get('/getAll', getAll);
+router.get('/', getAll);
 router.get('/:id', validateId, get);
 router.delete('/:id', isAuth, validateId, del);
 router.post('/upvote', isAuth, like);
